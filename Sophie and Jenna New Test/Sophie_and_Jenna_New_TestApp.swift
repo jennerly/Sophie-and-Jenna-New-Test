7a418ae5fba8f11a1f9db7ed898661d326fb12c5
Jenna <jennasaykhamphone.js@gmail.com>
2023-08-01 11:14:38 -0400

    Initial Commit

---
+++ Sophie and Jenna New Test/Sophie_and_Jenna_New_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sophie_and_Jenna_New_TestApp.swift
+//  Sophie and Jenna New Test
+//
+//  Created by scholar on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Sophie_and_Jenna_New_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
